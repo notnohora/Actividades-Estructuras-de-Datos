d0a8d151235dcf1b58be1c67bde022f30d1822f4
notnohora <nohora.cardozo@utp.edu.co>
2025-02-12 19:24:56 -0500

    primer commit

---
+++ hola.cc
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+#include <iostream>
+
+using namespace std;
+ int main(){
+    cout << "hello world..."<<endl;
+    return 0;
+=======
+#include <iostream>
+
+using namespace std;
+ int main(){
+    cout << "hello world"<<endl;
+    return 0;
+>>>>>>> f647cd1 (subiendo carpeta)
+ }
